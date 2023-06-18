3860af98d1b63741a8a19c3078c9513dcb977efd
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-06-18 23:29:01 +0500

    7

--- src/middlewares/inputvalidationmiddleware.ts
+++ src/middlewares/inputvalidationmiddleware.ts
@@ -10,14 +10,7 @@ import { errorsMessage, errorsType } from "../db/db";
 
 
 export const authorizationValidation = ((req: Request, res: Response, next:NextFunction) => {
-    const auth = {login: 'admin', password: 'qwerty'}
-    const b64auth = (req.headers.authorization || '').split('')[1] || ''
-        const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-    if (req.headers.authorization === 'Basic YWRtaW46cXdlcnR5' && login && password && login === auth.login && password === auth.password) {
-            return next()
-        }
-        res.set('WWW-Authenticate','Basic realm="401"')
-        res.status(401).send('Authentication required')
+    
     
 })
 
