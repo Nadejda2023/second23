6aa43054c82af4c8103d1f2e5eb940ec8416b9d0
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-06-18 23:02:00 +0500

    7

--- src/middlewares/inputvalidationmiddleware.ts
+++ src/middlewares/inputvalidationmiddleware.ts
@@ -9,13 +9,20 @@ import { errorsMessage, errorsType } from "../db/db";
 
 
 
-export const authorizationValidation = header('authorization').custom((value) => {
-    const user = usersRepository.find(user => user.loginPassword === value)
-    
-    if (!user) {
-        throw new Error('UNAUTHORIZED_401');
+export const authorizationValidation = ((req: Request, res: Response, next:NextFunction) => {
+    const auth = {login: 'admin', password: 'qwerty'}
+if (typeof req.headers.authorization !== "undefined") {
+    if ('Basic' === ((req.headers.authorization).split('')[0])) {
+
+        const b64auth = (req.headers.authorization).split('')[1] || ''
+        const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+        if (login && password && login === auth.login && password === auth.password) {
+            return next()
+        }
     }
-    return true
+}
+res.send(401)
 })
 
 export const inputBlogsValidation = {
