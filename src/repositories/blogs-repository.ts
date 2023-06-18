9ebd896f3a07dc7da1f7013ad8e70847e09cf7a7
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-06-18 23:43:50 +0500

    7

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -20,7 +20,7 @@ export const blogsRepository = {
     
     createBlog(name: string, description: string, website: string): blogsType {
         const newBlog: blogsType = {
-            id: (db.blogs.length + 1 ).toString(),   //db.blogs.length + 1         + new Date 
+            id: (+ new Date()).toString() ,   //db.blogs.length + 1         + new Date 
             name: name,
             description: description,
             websiteUrl: website
