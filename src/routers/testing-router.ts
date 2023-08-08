486ccc963dcdd632303935141309b09fa545bb7d
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-08-08 18:52:31 +0500

    1

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -4,8 +4,15 @@ import { postsRepository} from "../repositories/posts_db__repository";
 
 export const testingRouter = Router()
 
+testingRouter.delete('/all-data', (req: Request, res: Response) => {
+    blogsRepository.deleteAllBlogs()
+    postsRepository.deleteAllPosts()
+    res.status(204).send('All data is deleted')
+})
+/*export const testingRouter = Router()
+
   testingRouter.delete('/all-data', async (req: Request, res: Response) => {
   await Promise.all([blogsRepository.deleteAllBlogs(),
     postsRepository.deleteAllPosts()]);
     res.status(204).send('All data is deleted')
-})
+})*/
