b20faad681409355d66e0a4945a0aea3810baa6f
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-08-23 00:15:49 +0500

    bcrypt

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,18 +1,19 @@
 import {Request, Response, Router} from 'express'
 import { usersService } from '../domain/users-service'
+import { authorizationValidation } from '../middlewares/inputvalidationmiddleware'
 
 
 export const authRouter = Router({})
 
 authRouter.post('/login',
-
+authorizationValidation,//validation
 async ( req: Request, res: Response) => {
     const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
     if (!checkResult) {
-        res.sendStatus(400)
+        res.sendStatus(401)
         return
     } else {
-        res.status(204)
+        res.sendStatus(204)
     }
 }
 
