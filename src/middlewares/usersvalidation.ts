2ad64b8eba1e611933b400877f529f36362be2d9
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-09-05 23:08:23 +0500

    1

--- src/middlewares/usersvalidation.ts
+++ src/middlewares/usersvalidation.ts
@@ -9,6 +9,7 @@ const loginValidation = body('login')
                                             .trim()
                                             .notEmpty()
                                             .matches('^[a-zA-Z0-9_-]*$')
+                                            .withMessage('error login pattern')
                                             .isLength({min: 3, max: 10})
                                             .withMessage('Length must be from 3 to 10 simbols').custom(async (login) => {
 
@@ -36,6 +37,7 @@ const emailValidation = body('email')
                                             .isEmail()
                                             .withMessage('Must be  Email')
                                             .matches('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                                            .withMessage('error email pattern')
                                             .custom(async (email) => {
                                                 const user = await usersTwoRepository.findUserByEmail(email);
                                                 if(user){
